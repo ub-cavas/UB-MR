198224d08ab1028d306980bc655d609b3e1ae403
oakley-Thomas <oakleyth@buffalo.edu>
2025-06-26 12:06:07 -0400

    Added a button to toggle ghost vehicle and stats panel

---
+++ Assets/UB_MR/Scripts/UI/HUD.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] Button mHUD_Button;
+    Stats[] mStatPanels;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.mStatPanels = this.GetComponentsInChildren<Stats>(true);
+
+        this.mHUD_Button.onClick.AddListener(this.ToggleHUD);
+        this.mHUD_Button.onClick.AddListener(this.ToggleGhost);
+    }
+
+    void ToggleHUD()
+    {
+        // Stat Panels
+        foreach (Stats statPanel in this.mStatPanels)
+        {
+            if (statPanel != null)
+            {
+                statPanel.gameObject.SetActive(!statPanel.gameObject.activeSelf);
+            }
+        }
+    }
+
+    void ToggleGhost()
+    {
+        DT_Reflect ghost = Object.FindFirstObjectByType<DT_Reflect>(FindObjectsInactive.Include);
+        if (ghost != null)
+        {
+            ghost.gameObject.SetActive(!ghost.gameObject.activeSelf);
+        }
+    }
+}
