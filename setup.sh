f5c402eddf0aab7a7f0eadccb371ec75002b79a1
oakleyth <oakleyth@buffalo.edu>
2025-07-03 15:09:46 -0400

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -5,14 +5,16 @@ WORKSPACE_DIR="${WORKSPACE_DIR:-Externals/ros2_for_unity_ws}"
 UNITY_DIR="${WORKSPACE_DIR:-Assets}"
 
 # Install ros2-for-unity repo
-git clone https://github.com/oakley-Thomas/ros2-for-unity.git  "$WORKSPACE_DIR"
+if [ -d "$WORKSPACE_DIR" ]; then
+    echo "Found ros2-for-unity workspace"
+else
+    git clone https://github.com/oakley-Thomas/ros2-for-unity.git  "$WORKSPACE_DIR"
+fi
 cd "$WORKSPACE_DIR"
 git checkout virtual-object-detection
 
 # source ros2
 source /opt/ros/humble/setup.bash
-printenv | grep -i ROS
-
 
 ROS_DISTRO="${ROS_DISTRO:-humble}"
 REPOS_FILE_URL="https://raw.githubusercontent.com/RobotecAI/ros2-for-unity/develop/ros2_for_unity_custom_messages.repos"
@@ -41,12 +43,7 @@ bash pull_repositories.sh
 
 echo "-- Building Unity asset"
 bash build.sh --overlay 
-
-echo "-- Installing Unity asset"
-cd ..
-cd ..
-cp $WORKSPACE_DIR/install/asset/Ros2ForUnity "$UNITY_DIR"
-
+echo "Copy over the Ros2ForUnity folder from Externals/ros2_for_untiy_ws/install/asset"
 echo "Setup complete! To use overlay mode, remember to:"
 echo "  source /opt/ros/$ROS_DISTRO/setup.bash"
 echo "before launching the Unity Editor."
