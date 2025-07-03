997a75fa9233767474c5fa9917340c5371e9c3ef
oakleyth <oakleyth@buffalo.edu>
2025-07-03 14:54:31 -0400

    Created setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,52 @@
-# Clone ros2-for-unity repo to build custom Message Types
-git clone git@github.com:RobotecAI/ros2-for-unity.git ./Externals/ros2-for-unity
+#!/bin/bash
+set -e
 
+WORKSPACE_DIR="${WORKSPACE_DIR:-Externals/ros2_for_unity_ws}"
+UNITY_DIR="${WORKSPACE_DIR:-Assets}"
+
+# Install ros2-for-unity repo
+git clone https://github.com/oakley-Thomas/ros2-for-unity.git  "$WORKSPACE_DIR"
+cd "$WORKSPACE_DIR"
+git checkout virtual-object-detection
+
+# source ros2
 source /opt/ros/humble/setup.bash
-cd Externals/ros2-for-unity
+printenv | grep -i ROS
 
-# Pull new 
-./pull_repositories.sh
+
+ROS_DISTRO="${ROS_DISTRO:-humble}"
+REPOS_FILE_URL="https://raw.githubusercontent.com/RobotecAI/ros2-for-unity/develop/ros2_for_unity_custom_messages.repos"
+ROS2CS_REPOS_URL="https://raw.githubusercontent.com/RobotecAI/ros2-for-unity/develop/ros2cs.repos"
+
+echo "=== Ubuntu setup for ROS2 For Unity ($ROS_DISTRO) ==="
+
+echo "-- Update and install ROS2 dependencies"
+sudo apt update
+sudo apt install -y python3-pip
+sudo apt install python3-vcstool
+sudo apt install dotnet-sdk-7.0
+sudo apt install patchelf
+sudo apt install -y \
+  ros-"$ROS_DISTRO"-test-msgs \
+  ros-"$ROS_DISTRO"-fastrtps \
+  ros-"$ROS_DISTRO"-rmw-fastrtps-cpp 
+sudo apt update
+echo "=== DONE ($ROS_DISTRO) ==="
+
+echo "-- Preparing workspace directory at $WORKSPACE_DIR"
+
+echo "-- Importing repositories"
+chmod +x pull_repositories.sh
+bash pull_repositories.sh
+
+echo "-- Building Unity asset"
+bash build.sh --overlay 
+
+echo "-- Installing Unity asset"
+cd ..
+cd ..
+cp $WORKSPACE_DIR/install/asset/Ros2ForUnity "$UNITY_DIR"
+
+echo "Setup complete! To use overlay mode, remember to:"
+echo "  source /opt/ros/$ROS_DISTRO/setup.bash"
+echo "before launching the Unity Editor."
