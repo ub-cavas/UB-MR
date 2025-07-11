1e8d028b29b86e8a51145e2cff1b4ec08dd6dc51
Oakley Thomas <oakleyth@buffalo.edu>
2025-07-11 12:52:21 -0400

    Updated network configuration to support predictive digital twin client

--- Assets/UB_MR/Scripts/UI/Network/NetworkUI.cs
+++ Assets/UB_MR/Scripts/UI/Network/NetworkUI.cs
@@ -4,8 +4,12 @@ using Unity.Netcode;
 
 namespace CAVAS.UB_MR.UI.Network
 {
-    public class NetworkUI : MonoBehaviour
+    public class NetworkUI : NetworkBehaviour
     {
+        [SerializeField] GameObject mSpectatorPrefab;
+        [SerializeField] GameObject mPredictiveDigitalTwinPrefab;
+        [SerializeField] GameObject mGPSDigitalTwinPrefab;
+
         public void StartHost()
         {
             NetworkManager.Singleton.StartHost();
@@ -14,6 +18,54 @@ namespace CAVAS.UB_MR.UI.Network
         public void StartClient()
         {
             NetworkManager.Singleton.StartClient();
+        }
+
+        public void SpawnSpectator()
+        {
+            SpawnSpectatorServerRpc();
+        }
+
+        public void SpawnDigitalTwin(bool inPredictive)
+        {
+            if (inPredictive)
+            {
+                SpawnPredictiveDigitalTwinServerRpc();
+            }
+            else
+            {
+                SpawnGPSDigitalTwinServerRpc();
+            }
+        }
+        
+
+        [ServerRpc(RequireOwnership = false)]
+        void SpawnSpectatorServerRpc(ServerRpcParams rpcParams = default)
+        {
+            ulong callerClientId = rpcParams.Receive.SenderClientId;
+            NetworkSpawn(mSpectatorPrefab, callerClientId);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void SpawnPredictiveDigitalTwinServerRpc(ServerRpcParams rpcParams = default)
+        {
+            ulong callerClientId = rpcParams.Receive.SenderClientId;
+            NetworkSpawn(mPredictiveDigitalTwinPrefab, callerClientId);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void SpawnGPSDigitalTwinServerRpc(ServerRpcParams rpcParams = default)
+        {
+            ulong callerClientId = rpcParams.Receive.SenderClientId;
+            NetworkSpawn(mGPSDigitalTwinPrefab, callerClientId);
+        }
+
+        
+        void NetworkSpawn(GameObject prefab, ulong client_id)
+        {
+            Debug.Log("Spawning " + prefab.name + " for client " + client_id);
+            var instance = Instantiate(prefab);
+            var instanceNetworkObject = instance.GetComponent<NetworkObject>();
+            instanceNetworkObject.SpawnWithOwnership(client_id);
         }
     }
 }
