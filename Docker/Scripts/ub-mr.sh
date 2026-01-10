d2d0b8211ef7e7c57f627661cb6d1ebdd55bc1d4
Oakley Thomas <oakleyth@buffalo.edu>
2026-01-10 14:14:08 -0500

    Added back source ros2 line to ub-mr.sh

--- Docker/Scripts/ub-mr.sh
+++ Docker/Scripts/ub-mr.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# Pull and rebuild mr_pkg from origin/main
-cd /ub_mr_workspace/src/mr_pkg
-git fetch
-git pull
-cd /ub_mr_workspace
-colcon build
-
 # Run from /app (matches your WORKDIR)
 cd /app
 
@@ -15,7 +8,7 @@ cd /app
 chmod +x "${UB_MR_PLAYER_DIR}/UB-MR.x86_64"
 
 # Start Unity in the foreground (container lifetime == Unity lifetime)
-
+source /opt/ros/humble/setup.bash
 exec "${UB_MR_PLAYER_DIR}/UB-MR.x86_64" \
   -screen-fullscreen 0 \
   -screen-width 1920 \
